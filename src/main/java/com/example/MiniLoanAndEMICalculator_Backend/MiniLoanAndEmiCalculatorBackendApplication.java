eaa2c708ace13a8fdb2a22f5d77e9ce503161f87
Rohit Kumar <rohit_kumar@srmap.edu.in>
2025-12-06 22:43:43 +0530

    Basic steup of my backend projec

---
+++ src/main/java/com/example/MiniLoanAndEMICalculator_Backend/MiniLoanAndEmiCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniLoanAndEMICalculator_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLoanAndEmiCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniLoanAndEmiCalculatorBackendApplication.class, args);
+	}
+
+}
